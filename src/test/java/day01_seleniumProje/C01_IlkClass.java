fbb74f9e2af928bb9bdb3f15767f00f7229ce974
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-09 23:22:58 +0300

    09.05.2022

--- src/test/java/day01_seleniumProje/C01_IlkClass.java
+++ src/test/java/day01_seleniumProje/C01_IlkClass.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class day01_seleniumProje {
+package day01_seleniumProje;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkClass {
+    public static void main(String[] args) throws InterruptedException {
+       /* En ilkle haliyle bir otomasyonu yapmak için
+  Classımıza otomasyon için gerekli olan webdriver'in yerini göstermemniz gerekir.
+  bunun için java kutuphanesinden System.setProperty() methodunu kullaniriz...
+  method 2 parametre istemektedir.
+ --> ilki kullanacagımız driver : webdriver.chrome.driver
+ --> ikincisi ise bu diriverin fiziki yolu :  */
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com.tr/");
+        Thread.sleep(2000);
+
+
+    }
 }
